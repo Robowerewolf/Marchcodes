026006eba5418a36f7118557ae638465c1885c1c
game110pm <game110pm@605-110-STU21>
2017-03-01 13:21:53 -0700

    cfg

---
+++ Jukebox/Jukebox/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void jukebox(int x);
+int num;
+int input;
+int main() {
+	cout << "What sound do you want to hear? For yer a wizard, harry, press (1). For Invader Zim, press (2). For Sterling Archer, press (3). For Johnny Bravo, press (4)." << endl;
+	cin >> input;
+	jukebox(input);
+}
+void jukebox(int x) {
+	switch (x) {
+	case 1: 
+		PlaySound(TEXT("youreawizard.wav"), NULL, SND_FILENAME);
+		break;
+	case 2: 
+		PlaySound(TEXT("zim.wav"), NULL, SND_FILENAME );
+		break;
+	case 3:
+		PlaySound(TEXT("dangerZone2.wav"), NULL, SND_FILENAME);
+		break;
+	case 4:
+		PlaySound(TEXT("love.wav"), NULL, SND_FILENAME );
+		break;
+	}
+}
