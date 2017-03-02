b79db20dbeae88d312e932b8ae3e69a88be9444b
game110pm <game110pm@605-110-STU21>
2017-03-02 12:33:37 -0700

    dfgj

--- Jukebox/Jukebox/Source.cpp
+++ Jukebox/Jukebox/Source.cpp
@@ -2,7 +2,6 @@
 #include <windows.h>
 using namespace std;
 void jukebox(int x);
-int num;
 int input;
 int main() {
 	cout << "What sound do you want to hear? For yer a wizard, harry, press (1). For Invader Zim, press (2). For Sterling Archer, press (3). For Johnny Bravo, press (4)." << endl;
